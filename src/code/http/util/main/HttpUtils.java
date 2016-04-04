8909c46ebce0da332d09d22e6b8421fbe87481cb
Vineeth <vineeth.madhu@gmail.com>
2016-04-05 00:46:57 +0530

    Overrode callGetUrl.
    For Get requests which do not need header values.

--- src/code/http/util/main/HttpUtils.java
+++ src/code/http/util/main/HttpUtils.java
@@ -1,6 +1,6 @@
- package code.http.util.main;
- 
- import java.io.BufferedReader;
+package code.http.util.main;
+
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URLEncoder;
@@ -17,10 +17,9 @@ import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClients;
 import org.apache.http.message.BasicNameValuePair;
- 
- public class HttpUtils
- {
-   
+
+public class HttpUtils {
+	// Get Request, where header is used
 	public String callGETUrl(String url, Map<String, String> headerMap) {
 		String response = "";
 		StringBuffer result = new StringBuffer();
@@ -53,6 +52,7 @@ import org.apache.http.message.BasicNameValuePair;
 			ioException.printStackTrace();
 			return "An IOException Protocol Exception has occured.\n" + ioException.toString();
 		} catch (Exception someException) {
+			// Houston we have a problem
 			someException.printStackTrace();
 			return "An Exception has occured.\n" + someException.toString();
 		} finally {
@@ -67,14 +67,59 @@ import org.apache.http.message.BasicNameValuePair;
 			}
 		}
 	}
-   
-	public String postData(String url, String data,
-			Map<String, String> headerMap) {
+
+	// Get Request where header is not used.
+	public String callGETUrl(String url) {
+		String response = "";
+		StringBuffer result = new StringBuffer();
+		CloseableHttpClient httpClient = null;
+		try {
+			httpClient = HttpClients.createDefault();
+			if (url.startsWith("https")) {
+				httpClient = (CloseableHttpClient) HttpsClientWrapper.getHttpClient();
+			}
+			HttpGet getRequest = new HttpGet(url);
+			HttpResponse httpResponse = httpClient.execute(getRequest);
+			if (httpResponse.getStatusLine().getStatusCode() != 200) {
+				throw new RuntimeException(
+						"Failed : HTTP error code : " + httpResponse.getStatusLine().getStatusCode());
+			}
+			BufferedReader br = new BufferedReader(new InputStreamReader(httpResponse.getEntity().getContent()));
+			String output;
+			while ((output = br.readLine()) != null) {
+				result.append(output);
+			}
+			response = result.toString();
+			return response;
+		} catch (ClientProtocolException clientProtocolException) {
+			clientProtocolException.printStackTrace();
+			return "A Client Protocol Exception has occured.\n" + clientProtocolException.toString();
+		} catch (IOException ioException) {
+			ioException.printStackTrace();
+			return "An IOException Protocol Exception has occured.\n" + ioException.toString();
+		} catch (Exception someException) {
+			// Houston we have a problem
+			someException.printStackTrace();
+			return "An Exception has occured.\n" + someException.toString();
+		} finally {
+			if (httpClient != null && httpClient.getConnectionManager() != null) {
+				try {
+					httpClient.close();
+				} catch (IOException ioException) {
+					ioException.printStackTrace();
+					return "An IOException has occured while trying to close the connection.\n"
+							+ ioException.toString();
+				}
+			}
+		}
+	}
+
+	public String postData(String url, String data, Map<String, String> headerMap) {
 		String result = "";
 		CloseableHttpClient httpClient = null;
 		try {
 			HttpPost postRequest = new HttpPost(url);
-			
+
 			httpClient = HttpClients.createDefault();
 			if (url.startsWith("https")) {
 				httpClient = (CloseableHttpClient) HttpsClientWrapper.getHttpClient();
@@ -88,8 +133,7 @@ import org.apache.http.message.BasicNameValuePair;
 			urlParameters.add(new BasicNameValuePair("userInfo", URLEncoder.encode(data)));
 			postRequest.setEntity(new StringEntity(data));
 			HttpResponse response = httpClient.execute(postRequest);
-			BufferedReader rd = new BufferedReader(new InputStreamReader(
-					response.getEntity().getContent()));
+			BufferedReader rd = new BufferedReader(new InputStreamReader(response.getEntity().getContent()));
 			StringBuffer combinedResult = new StringBuffer();
 			String line = "";
 			while ((line = rd.readLine()) != null) {
@@ -118,4 +162,4 @@ import org.apache.http.message.BasicNameValuePair;
 		}
 		return result;
 	}
- }
+}
