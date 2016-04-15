f8724a89b27cf23816a2af36b2f054cdc6ab244e
Vineeth <vineeth.madhu@gmail.com>
2016-04-15 22:49:19 +0530

    Added Classes Config and ConfigReader
    Added the classes config and configReader to read the proxy data.

---
+++ src/code/http/utils/proxy/Config.java
@@ -0,0 +1,59 @@
+package code.http.utils.proxy;
+
+import java.util.Properties;
+
+public class Config {
+	private static String propertiesFileName = "E:\\Git Hub Code\\HttpUtils\\HttpUtils-master\\config.properties";
+
+	private static Properties init() {
+		Properties prop = new Properties();
+		return prop;
+	}
+
+	private static String getConfig(String propName) {
+		ConfigReader config = new ConfigReader(propertiesFileName);
+		config.constantProperties(init(), "Properties Files");
+		return config.readPropertyFile(propName);
+
+	}
+
+	/**
+	 * @return the proxyRequired
+	 */
+	public static Boolean getProxyRequired() {
+		String retStr = getConfig("proxyrequired").trim();
+		if (retStr.equals("-1"))
+			return false;
+		else {
+			return Boolean.parseBoolean(retStr);
+		}
+	}
+
+	/**
+	 * @return the proxyHost
+	 */
+	public static String getProxyIp() {
+		return getConfig("proxyIp").trim();
+	}
+
+	/**
+	 * @return the proxyUserName
+	 */
+	public static String getUsername() {
+		return getConfig("username").trim();
+	}
+
+	/**
+	 * @return the proxyPassword
+	 */
+	public static String getPassword() {
+		return getConfig("password").trim();
+	}
+
+	/**
+	 * @return the proxyPort
+	 */
+	public static String getPort() {
+		return getConfig("port").trim();
+	}
+}
---
+++ src/code/http/utils/proxy/ConfigReader.java
@@ -0,0 +1,64 @@
+package code.http.utils.proxy;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+public class ConfigReader {
+	Properties propertiesFile;
+	private String propertiesFileName;
+
+	public ConfigReader (String propFileName) {
+		propertiesFileName = propFileName;
+	}
+	
+	public void constantProperties(
+			Properties connectionProperties,
+			String description) {
+		
+			File config = new File(propertiesFileName);
+			if (config.exists() && config.isFile()) {
+				Properties properties = new Properties();
+				try {
+					properties.load(new FileInputStream(config));
+				} catch (FileNotFoundException propertiesException) {
+					System.err.println(propertiesException);
+				} catch(IOException ioException){
+					System.err.println(ioException);
+				} catch (NullPointerException nullPointer){
+					System.err.println(nullPointer);
+				} catch (Exception someOtherException){
+					System.err.println(someOtherException);
+				}
+				propertiesFile = properties;
+				return;
+			}
+			try {
+				FileOutputStream outputStream = new FileOutputStream(config, false);
+				connectionProperties.store(outputStream, description);
+				outputStream.close();
+			} catch (FileNotFoundException fileNotFound){
+				fileNotFound.printStackTrace();
+			} catch(NullPointerException nullpointer){
+				nullpointer.printStackTrace();
+			} catch(IOException ioException){
+				ioException.printStackTrace();
+			} catch(Exception someOtherException){
+				someOtherException.printStackTrace();
+			}
+			propertiesFile = connectionProperties;
+	}
+
+	public String readPropertyFile(String propName){
+		try {
+		  return propertiesFile.getProperty(propName);
+		} catch (NullPointerException fileNotFound){
+			fileNotFound.printStackTrace();
+		} catch(Exception someOtherException){
+			someOtherException.printStackTrace();
+		}
+		return "-1";
+	}
+}
